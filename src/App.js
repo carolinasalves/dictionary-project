1c089c20332716f1a02227b8fe82c43cbdca4254
carolinasalves <carolinasalves04@gmail.com>
2022-07-18 20:57:28 +0100

    Added synonyms

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App text-center">
+    <div className="App">
       <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
