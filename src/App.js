473be19771a61bbb8dd1a346a946f6f270af0c4a
carolinasalves <carolinasalves04@gmail.com>
2022-07-29 13:47:12 +0100

    Changed the header logo to a "h1"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -7,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>📚 Dictionary</h1>
         </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
